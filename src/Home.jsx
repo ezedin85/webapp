d87a5301db6afc17fa7e758350489f69047f403c
Ezedin <ezedinnuru85@gmail.com>
2024-01-17 11:58:10 +0300

    valid

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,15 +1,18 @@
 const tele = window.Telegram.WebApp;
 
 tele.MainButton.show()
-tele.MainButton.onClick(()=>tele.showAlert("Hello from Clizle"))
-const a = tele.colorScheme
+tele.MainButton.onClick(() => tele.showAlert("Hello from Clizle"))
+const usersTheme = tele.colorScheme
 function Home() {
-  return (
-    <div>
-        {a}
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod dolorum, voluptas adipisci pariatur eos modi earum! Molestiae beatae laudantium consequatur!
-    </div>
-  )
+
+    const style = usersTheme === 'light' ? { backgroundColor: 'white', color: 'black' } : 
+    { backgroundColor: 'black', color: 'white' } 
+
+    return (
+        <div style={style}>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod dolorum, voluptas adipisci pariatur eos modi earum! Molestiae beatae laudantium consequatur!
+        </div>
+    )
 }
 
 export default Home;
