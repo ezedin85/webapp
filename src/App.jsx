75a8f19abe09153810c736b23025257eea542954
Ezedin <ezedinnuru85@gmail.com>
2024-01-17 11:51:26 +0300

    valid

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Home from "./Home";
 
+
 function App() {
   return ( <Home/>)
 }
--- src/Home.jsx
+++ src/Home.jsx
@@ -2,9 +2,11 @@ const tele = window.Telegram.WebApp;
 
 tele.MainButton.show()
 tele.MainButton.onClick(()=>tele.showAlert("Hello from Clizle"))
+const a = tele.colorScheme
 function Home() {
   return (
     <div>
+        {a}
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod dolorum, voluptas adipisci pariatur eos modi earum! Molestiae beatae laudantium consequatur!
     </div>
   )
