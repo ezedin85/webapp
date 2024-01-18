4be2d19f258c6a329863c64becfe7fa02afc78d2
Ezedin <ezedinnuru85@gmail.com>
2024-01-18 12:42:16 +0300

    Your commit message

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,18 +1,28 @@
 const tele = window.Telegram.WebApp;
+import { Link } from "react-router-dom";
 
-tele.MainButton.show()
-tele.MainButton.onClick(() => tele.showAlert("Hello from Clizle"))
-const usersTheme = tele.colorScheme
+tele.MainButton.show();
+tele.SettingsButton.show();
+tele.BackButton.show();
+tele.MainButton.setText("ክፈል");
+tele.MainButton.showProgress(false);
+const startcom = tele.WebAppInitData?.start_param
+// tele.MainButton.color = "red";
+tele.MainButton.onClick(() => tele.showAlert("Hello rom Clizle"));
+const usersTheme = tele.colorScheme;
 function Home() {
+  const style =
+    usersTheme === "light"
+      ? { backgroundColor: "white", color: "black" }
+      : { backgroundColor: "black", color: "white" };
 
-    const style = usersTheme === 'light' ? { backgroundColor: 'white', color: 'black' } : 
-    { backgroundColor: 'black', color: 'white' } 
-
-    return (
-        <div style={style}>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod dolorum, voluptas adipisci pariatur eos modi earum! Molestiae beatae laudantium consequatur!
-        </div>
-    )
+  return (
+    <div style={style}>
+      oor st {startcom} 13 consectetur, adipisicing elit
+      9 {tele.MainButton?.toString()}
+      <Link to="/about">Go Abou</Link>
+    </div>
+  );
 }
 
 export default Home;
